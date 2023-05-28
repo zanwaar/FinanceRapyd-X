80bb6b9a496c55eee342a123fa5c969881e05310
zanwaar <zanwaarsamjid@gmail.com>
2023-05-28 11:54:14 +0800

    Update Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,3 @@
-import { ArrowForwardIcon } from "@chakra-ui/icons";
 import {
   Avatar,
   Box,
@@ -14,26 +13,36 @@ import { FiLogOut } from "react-icons/fi";
 
 export const Header = () => {
   const { data: session, status } = useSession();
-  const loading = status === "loading";
   return (
     <>
       {session && (
-        <Box bg="white" p="5">
+        <Box bg="white" py={{ base: "2", md: "5" }} px={{ base: "4", md: "0" }}>
           <Flex maxW={"3xl"} alignItems="center" gap="2">
             <Avatar
-              size={{ base: "sm", md: "md" }}
-              name="Dan Abrahmov"
+              size={{ base: "xs", md: "md" }}
+              name={session.user.name}
               src={session.user.image}
             />
             <Box p="2">
-              <Heading fontSize={{ base: "10px", md: "18px" }}>
+              <Heading fontSize={{ base: "xs", md: "md" }}>
                 {session.user.email ?? session.user.name}
               </Heading>
             </Box>
             <Spacer />
-
+            <Box
+              as="a"
+              display={{ base: "block", md: "none" }}
+              borderRadius="full"
+              onClick={(e) => {
+                e.preventDefault();
+                signOut();
+              }}
+            >
+              <FiLogOut />
+            </Box>
             <Button
-              size={{ base: "sm", md: "md" }}
+              display={{ base: "none", md: "flex" }}
+              size="md"
               rightIcon={<FiLogOut />}
               colorScheme="teal"
               variant="outline"
